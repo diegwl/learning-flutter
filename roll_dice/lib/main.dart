17c6f2a40ac76e7e3c87659926af8e11a20f131f
diegwl <diegocastanlopes0210@outlook.com>
2024-02-02 16:59:49 -0300

    dice roll app

--- roll_dice/lib/main.dart
+++ roll_dice/lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice/gradient_container.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello World!')));
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+          ],
+        ),
+      ),
+    ),
+  );
 }
